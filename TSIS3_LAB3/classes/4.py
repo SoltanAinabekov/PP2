c63cca4461e9e4a79cdd6438443cae7a9df1efc5
SoltanAinabekov <ainabekovbs@gmail.com>
2025-02-02 17:35:46 +0500

    Update 4.py

--- TSIS3_LAB3/classes/4.py
+++ TSIS3_LAB3/classes/4.py
@@ -6,12 +6,4 @@ class Point:
         self.x = x
         self.y = y
 
-    def show(self):
-        print(f"Point({self.x}, {self.y})")
-
-    def move(self, x, y):
-        self.x = x
-        self.y = y
-
-    def dist(self, other_point):
-        return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+q=Point(0,0)
